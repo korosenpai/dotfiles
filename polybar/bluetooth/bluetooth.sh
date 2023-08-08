a20d147a797a30cb02fdff3b7a5914d8e4774b53
korosenpai <koro.dev404@gmail.com>
2023-08-09 00:30:43 +0200

    polybar

---
+++ polybar/bluetooth/bluetooth.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
+then
+  echo "%{F#66ffffff}󰂲 off"
+else
+  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
+  then 
+    echo " on"
+  fi
+  # connected to device
+  #devicename= $(bluetoothctl devices | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done|grep -e "Device\|Connected\|Name" | grep "Name:" | cut -d " " -f 2-)
+  echo -n " "
+  bluetoothctl devices | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done|grep -e 'Device\|Connected\|Name' | grep 'Name:' | cut -d ' ' -f 2-
+fi
