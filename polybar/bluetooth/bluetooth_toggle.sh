1d450ccf7917dc0417c0c9f7fbb1005d48319ece
korosenpai <koro.dev404@gmail.com>
2023-08-13 14:29:41 +0200

    bluetooth improvements

--- polybar/bluetooth/bluetooth_toggle.sh
+++ polybar/bluetooth/bluetooth_toggle.sh
@@ -2,6 +2,8 @@
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
   bluetoothctl power on
+  notify-send "bluetooth ON"
 else
   bluetoothctl power off
+  notify-send "bluetooth OFF"
 fi
