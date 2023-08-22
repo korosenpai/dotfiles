5b28e8bf41a0cf5aa12ca5293143862fb7a988f3
korosenpai <koro.dev404@gmail.com>
2023-08-22 08:01:05 +0200

    also added support for bloc num

--- .utils/special_keys/num_lock_status.sh
+++ .utils/special_keys/num_lock_status.sh
@@ -2,8 +2,8 @@
 #xset q | grep -oE "00: Caps Lock:[ ]{1,20}o[fn]{1,2}" | grep -oE "o[fn]{1,2}"
 
 # caps lock effect has not taken place yet and saved so wait
-sleep 0.2
+sleep 0.3
 
-status=$(xset q | grep -oE "00: Caps Lock:[ ]{1,20}o[fn]{1,2}" | grep -oE "o[fn]{1,2}")
-notify-send "Caps Lock: $status"
+status=$(xset q | grep -oE "01: Num Lock:[ ]{1,20}o[fn]{1,2}" | grep -oE "o[fn]{1,2}")
+notify-send "Num Lock: $status"
 echo $status
