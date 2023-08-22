f6c6470a0ed1944cb12eb5814fc09653f38cf660
korosenpai <koro.dev404@gmail.com>
2023-08-22 07:54:30 +0200

    increased delay for more precise result

---
+++ .utils/special_keys/caps_lock_status.sh
@@ -0,0 +1,9 @@
+#echo -n "Caps Lock: "
+#xset q | grep -oE "00: Caps Lock:[ ]{1,20}o[fn]{1,2}" | grep -oE "o[fn]{1,2}"
+
+# caps lock effect has not taken place yet and saved so wait
+sleep 0.3
+
+status=$(xset q | grep -oE "00: Caps Lock:[ ]{1,20}o[fn]{1,2}" | grep -oE "o[fn]{1,2}")
+notify-send "Caps Lock: $status"
+echo $status
