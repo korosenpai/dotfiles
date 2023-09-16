60415db16a858c4e682d861736df2f0be9cc4987
korosenpai <koro.dev404@gmail.com>
2023-09-16 16:11:44 +0200

    shift s to open grep string in telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,7 +2,6 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {}) -- all file search
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})       -- git file search with ctrl + p
--- TODO: find better mapping for  grep_string
-vim.keymap.set('n', '<leader>sr', function()
+vim.keymap.set('n', '<S-s>', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ")})
 end) -- project search
