eb557e18f91ad131ddd87deb677aad0d32d6895a
korosenpai <koro.dev404@gmail.com>
2024-01-27 13:10:26 +0100

    fixed updated_wlan

--- polybar/wlan.sh
+++ polybar/wlan.sh
@@ -3,13 +3,14 @@
 # Check if any wireless devices are blocked (airplane mode)
 if rfkill list wlan | grep -q "blocked: yes"; then
     echo "󰀝 airplane"
+    exit
+fi
 
-elif nmcli device status | grep wifi | grep disconnected > /dev/null; then
-    echo "󰖪 offline"
+prefix="Auto " # prefix to remove when showing wifi on bar
+ssid=$(nmcli -t -f NAME c show --active | sed -e "s/^prefix//")
 
-else 
-    prefix="Auto " # prefix to remove when showing wifi on bar
-    ssid=$(nmcli -t -f NAME c show --active | sed -e "s/^$prefix//")
+if [ -n "$ssid" ]; then
     echo "  $ssid" 
-
+else
+    echo "󰖪 offline"
 fi
