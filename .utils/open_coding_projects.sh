bf064fa67d07543f44efb0c502e6c3aa3f1d31a2
korosenpai <koro.dev404@gmail.com>
2024-02-13 21:37:01 +0100

    better way to open projects similiar to the tmux one

---
+++ .utils/open_coding_projects.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# open folders saved by neovim telescope projects
+
+projects_file="$HOME/.local/share/nvim/telescope-projects.txt"
+
+# check if project saved or disabled
+# active: koro=/home/koro=w0=1=1 -> add in fzf
+# deactive: koro=/home/koro=w0=0=1 -> not include
+# =1= only 1 in middle is the one we want
+folders=$(grep '=1=' $projects_file \
+    | awk -F '=' '{ print $2 }') # cut with "=" and get second field
+
+
+selected=$(echo "$folders" | fzf)
+if [ $? -ne 0 ];
+then
+    echo no projects selected
+    exit
+fi
+
+
+cd $selected
+# exec neovim and then exec zsh to keep it open after neovmi exits
+gnome-terminal -- zsh -c "nvim . ; exec zsh"
+gnome-terminal .
