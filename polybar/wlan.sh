c4c98390c1b579ede6a6deb9706576747a96a04d
korosenpai <koro.dev404@gmail.com>
2024-03-01 07:32:36 +0100

    removed docker0 showing when not connected to wifi

--- polybar/wlan.sh
+++ polybar/wlan.sh
@@ -7,7 +7,7 @@ if rfkill list wlan | grep -q "blocked: yes"; then
 fi
 
 prefix="Auto " # prefix to remove when showing wifi on bar
-ssid=$(nmcli -t -f NAME c show --active | sed -e "s/^prefix//")
+ssid=$(nmcli -t -f NAME c show --active | grep -v "docker0" | sed -e "s/^prefix//")
 
 if [ -n "$ssid" ]; then
     echo "  $ssid" 
